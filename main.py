a7f1879884daa697429b1a1e8d6a726e34a81532
benjanator805 <benramirez805@gmail.com>
2024-06-30 20:48:36 -0700

    main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
+    
     ########################################
-    Code Your Program here
+    if num1 <= num2 and num1 <= num3:
+        minval = num1
+    elif num2 <= num3 and num2 <= num1:
+        minval = num2
+    elif num3 <= num1 and num3 <= num2:
+        minval = num3
+    
+    
     ########################################
-    """
+    
 
     print(minval, median, maxval)
     ########################################
